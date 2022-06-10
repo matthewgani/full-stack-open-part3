03433a400a80f67f4f96a9d555395eaef89cbc5f
matthewgani <matthewgani2@gmail.com>
2022-06-10 15:55:54 +0800

    Finish 3.15

--- index.js
+++ index.js
@@ -36,10 +36,17 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
     
-    response.status(204).end()
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
+    
+    // response.status(204).end()
 })
 
 const generateId = () => {
