1a799fa9f18b6570f0e53d6d2319c7d5a37704a3
matthewgani <matthewgani2@gmail.com>
2022-06-10 15:19:08 +0800

    Finish 3.14, tested with front end

--- index.js
+++ index.js
@@ -20,30 +20,6 @@ morgan.token('post', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 
 app.get('/api/persons/:id', (request, response) => {
     let id = Number(request.params.id)
