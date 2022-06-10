5a07fecd76f3c3f26c36b758ce3eb7fd9975e87a
matthewgani <matthewgani2@gmail.com>
2022-06-10 16:43:59 +0800

    Finish 3.17

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const { trusted } = require('mongoose')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -16,26 +17,26 @@ morgan.token('post', function (req, res) {
     if (req.method === 'POST') {
         return JSON.stringify(req.body)
     }
+    // can also check if error code is 400, return the error/json
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 
-app.get('/api/persons/:id', (request, response) => {
-    let id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    // console.log(typeof(person))
-    if (person === undefined) {
-        console.log('no person found')
-        return response.status(404).end()
-
-    } 
-
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
     Person.findByIdAndRemove(request.params.id)
     .then(result => {
@@ -49,30 +50,37 @@ app.delete('/api/persons/:id', (request, response) => {
     // response.status(204).end()
 })
 
-const generateId = () => {
-    min = 1
-    max = Number.MAX_SAFE_INTEGER
-
-    return Math.floor(Math.random() * (max-min) + min)
-
-}
-
+// NEED TO REWORK
 const checkForDuplicateName = (name) => {
 
-    const person = persons.find(person => person.name === name)
-    if (person === undefined) {
+    // const person = persons.find(person => person.name === name)
+    // if (person === undefined) {
+    //     return false
+    // }
+    // else {
+    //     return true
+    // }
+    let res = false
+    Person.find({}).then(persons => {
+        
+        persons.forEach((person)=> {
+            // console.log(person)
+            if (person.name === name) {
+                console.log('dup')
+                res = true
+            }
+        })
+        console.log('res', res)
+        return res
+    }).catch((error) => {
+        next(error)
         return false
-    }
-    else {
-        return true
-    }
+    })
 }
 // add the new person and return the json object created
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     // uses json parser
-
-
     // console.log(body)
     if (!body.name || !body.number) {
         return response.status(400).json({ 
@@ -80,40 +88,71 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (checkForDuplicateName(body.name)){
-        return response.status(400).json({
-            error: 'name must be unique'
+    // in the front end, we already check for dups
+    // but if we use POSTMAN, it does not go through the front end
+    Person.find({}).then(persons => {
+        let res = false
+        persons.forEach((person)=> {
+            // console.log(person)
+            if (person.name === body.name) {
+                res = true
+            }
         })
-    }
-    const person = new Person({
-        name: body.name,
-        number: body.number
+        if (res === false) {
+            const person = new Person({
+                name: body.name,
+                number: body.number
+            })
+        
+            person.save().then(savedPerson => {
+                // only return json as response once the person is saved into db
+                response.json(savedPerson)
+            })
+        }
+        else {
+            console.log('dup name')
+            return response.status(400).json({
+            error: 'name must be unique'
+            })
+        }
+    }).catch((error) => {
+        next(error)
     })
 
-    person.save().then(savedPerson => {
-        // only return json as response once the person is saved into db
-        response.json(savedPerson)
-    })
 
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     // we configure the personSchema in person.js
     // to return from Person model the object we want (without _id and _v)
     // to here
-    console.log('ee')
     Person.find({}).then(persons => {
         response.json(persons)
     }).catch((error) => {
-        console.log('error finding from MongoDB:', error.message)
+        next(error)
     })
 })
 
-app.get('/info', (request, response) => {
-    let responseString = `<p>Phonebook has info for ${persons.length} people </p>` 
-        + `<p>${new Date()}</p>`
-    response.send(responseString)
-})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+// app.get('/info', (request, response) => {
+//     let responseString = `<p>Phonebook has info for ${persons.length} people </p>` 
+//         + `<p>${new Date()}</p>`
+//     response.send(responseString)
+// })
 
 const PORT = process.env.PORT
 app.listen(PORT)
@@ -121,7 +160,24 @@ console.log(`Server running on port ${PORT}`)
 
 
 // to catch non existent routes, have to be after all routes
+// but be before the error handler
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    
+    // goes to default express error handler
+    next(error)
+}
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
